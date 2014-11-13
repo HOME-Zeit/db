a2d81e1299a943f6a5027ce7ba63697c6e7ef56f
Bob <BobBobovitz@users.noreply.github.com>
2014-11-13 16:57:19 +0100

    Update Test.java

--- Test.java
+++ Test.java
@@ -8,9 +8,9 @@ public class Test
 	{
 		Programminformation p  = new Programminformation(0, "ABC", System.currentTimeMillis(), System.currentTimeMillis() + 500000, "Montgomery", "Burns", System.currentTimeMillis() - 10000, System.currentTimeMillis() - 5000);
 		Datenbank.eintragen(p, true);
-		Datenbank.ausgeben(true);
-		Datenbank.ausgeben(System.currentTimeMillis(), true);
-		Datenbank.ausgeben(1, true);
+		ArrayList<Programminformation> p1 = Datenbank.ausgeben(true);
+		ArrayList<Programminformation> p2 = Datenbank.ausgeben(System.currentTimeMillis(), true);
+		ArrayList<Programminformation> p3 = Datenbank.ausgeben(1, true);
 		p.titel = "ABCD";
 		Datenbank.bearbeiten(p, true);
 		System.out.println("xxxxxxxxxxxxxxxxxxx");
