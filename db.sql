01a74fd012f812eb77ea9403974fb0d29b9c7e6a
Bob <BobBobovitz@users.noreply.github.com>
2014-11-18 13:49:24 +0100

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,12 +1,16 @@
+-- tabelle löschen
 drop table programminformation;
 
+
+
+-- tabelle anlegen
 create table programminformation(
 	nummer integer auto_increment,
 	titel varchar(150),
 	startzeit BIGINT,
 	endzeit BIGINT,
-	vorname varchar(150),
-	nachname varchar(150),
+	prod_verantwortlicher varchar(150),
+	sende_verantwortlicher varchar(150),
 	reale_startzeit BIGINT,
 	reale_endzeit BIGINT,
 	primary key (nummer)
